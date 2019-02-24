3dd13adef22006045356e9f484eea06171c6fcae
Виктор <spayn112@gmail.com>
2019-02-25 01:34:52 +0400

    Add phpdoc and change params to private

--- src/ThumbnailGenerator.php
+++ src/ThumbnailGenerator.php
@@ -9,16 +9,34 @@ use Imagine\Imagick\Imagine;
 class ThumbnailGenerator
 {
 
-    public $save_path;
-    public $url;
+    /**
+     * Path to save thumbnails
+     */
+    private $save_path;
 
-    public $resolutions = [
+    /**
+     * Url to thumbnails
+     */
+    private $url;
+
+    /**
+     * List resolutions
+     * label => resolution
+     */
+    private $resolutions = [
         'small' => '75x75',
         'medium' => '730x410'
     ];
 
 
-    public function __construct($save_path, $url, array $resolutions = [])
+    /**
+     * ThumbnailGenerator constructor.
+     *
+     * @param string $save_path
+     * @param string $url
+     * @param array $resolutions
+     */
+    public function __construct(string $save_path, string $url, array $resolutions = [])
     {
         $this->save_path = rtrim($save_path, DIRECTORY_SEPARATOR);
         $this->url = $url;
@@ -28,14 +46,28 @@ class ThumbnailGenerator
     }
 
 
-    public function getFileUrl($file_url, $thumb_directory, $prefix)
+    /**
+     * return thumbnail image url
+     *
+     * @param string $file_url
+     * @param string $thumb_directory
+     * @param string $prefix
+     * @return string
+     */
+    public function getFileUrl(string $file_url, string $thumb_directory, string $prefix)
     {
         $ext = pathinfo($file_url)['extension'];
         return $this->url . '/' . $thumb_directory . '/' . $prefix . '.' . $ext;
     }
 
 
-    public function generate($file, $thumb_directory)
+    /**
+     * Generate thumbnails
+     *
+     * @param string $file
+     * @param string $thumb_directory
+     */
+    public function generate(string $file, string $thumb_directory)
     {
         $ext = pathinfo($file)['extension'];
         $this->createDirectory($this->save_path . '/' . $thumb_directory);
@@ -47,7 +79,14 @@ class ThumbnailGenerator
     }
 
 
-    public function deleteThumbnailsDirectory($thumb_directory)
+    /**
+     * Delete thumbnails directory
+     *
+     * @param string $thumb_directory
+     *
+     * @return boolean
+     */
+    public function deleteThumbnailsDirectory(string $thumb_directory)
     {
         $dir = $this->save_path . DIRECTORY_SEPARATOR . $thumb_directory;
         if (file_exists($dir)) {
@@ -57,7 +96,15 @@ class ThumbnailGenerator
     }
 
 
-    private function generateThumbnail($file, $save_path, $width, $height)
+    /**
+     * Crop, resize and save thumbnail
+     *
+     * @param string $file
+     * @param string $save_path
+     * @param integer $width
+     * @param integer $height
+     */
+    private function generateThumbnail(string $file, string $save_path, integer $width, integer $height)
     {
         (new Imagine())
             ->open($file)
@@ -66,14 +113,28 @@ class ThumbnailGenerator
     }
 
 
-    private function getThumbnailPath($thumb_directory, $prefix, $extension)
+    /**
+     * Get path to thumbnail image
+     *
+     * @param string $thumb_directory
+     * @param string $prefix
+     * @param string $extension
+     * @return string
+     */
+    private function getThumbnailPath(string $thumb_directory, string $prefix, string $extension)
     {
         $new_filename = "$prefix." . $extension;
         return $this->save_path . DIRECTORY_SEPARATOR . $thumb_directory . DIRECTORY_SEPARATOR . $new_filename;
     }
 
 
-    private function createDirectory($directory)
+    /**
+     * Creates a directory recursively
+     *
+     * @param string $directory
+     * @return bool
+     */
+    private function createDirectory(string $directory)
     {
         if (file_exists($directory)) {
             return true;
@@ -82,7 +143,13 @@ class ThumbnailGenerator
     }
 
 
-    private function removeDirectory($dir)
+    /**
+     * Deletes a directory recursively
+     *
+     * @param $dir
+     * @return bool
+     */
+    private function removeDirectory(string $dir)
     {
         if ($objs = glob($dir."/*")) {
             foreach($objs as $obj) {
